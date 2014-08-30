d6f09109fc44e95b47b4dfbc8402044447b99a90
Masahito Yoshida <masahito@axsh.net>
2014-08-30 17:53:38 +0900

    first commit

---
+++ config.d/base.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# requires:
+#  bash
+#
+set -e
+set -o pipefail
+set -x
+
+# Do some changes ...
+
+name=kompira
+version=1.4.0
+
+basename=${name}-${version}-bin
+filename=${basename}.tar.gz
+
+cd /tmp
+
+if ! [[ -f /tmp/${filename} ]]; then
+  curl -fSkL https://bitbucket.org/kompira/package/downloads/${filename} -o ${filename}
+fi
+
+if ! [[ -d ${basename} ]]; then
+  tar zxvf ${filename}
+fi
+
+cd ${basename}
+sudo ./install.sh
